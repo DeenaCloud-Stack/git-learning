baafe7a0f200f2992b45da0582db53ff69a4707a
DeenaCloud-Stack <mailtodeenadayalan@gmail.com>
2023-06-18 14:49:29 +0400

    Hello World

---
+++ day3/functions.js
@@ -0,0 +1,16 @@
+//console.log('hello');
+
+function sayHello() {
+    console.log("Hello")
+}
+sayHello();
+
+function addTwoNumbers(x, y) {
+    console.log(x + y);
+}
+addTwoNumbers(5, 6);
+
+function add(x, y) {
+    return x + y;
+}
+console.log(add(add(5, 6), 11));
